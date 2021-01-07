b4808d943c5398df378799a3d99aad63fa36e8bf
rtambunt <rtambunting@gmail.com>
2021-01-06 17:39:11 -0800

    Fix correct wake up time

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ const btnAdd = document.querySelector('.btn-add');
 const bedtimeEl = document.querySelector('.bedtime');
 const lengthEl = document.querySelector('.length');
 const goalDisplay = document.querySelector('.goal-display');
+const isPM = true;
 let bedtime;
 let length;
 
 btnAdd.addEventListener('click', function () {
-  bedtime = bedtimeEl.value;
-  length = lengthEl.value;
+  bedtime = Number(bedtimeEl.value);
+  length = Number(lengthEl.value);
+  const wakeUpTime =
+    bedtime + length > 12 ? bedtime + length - 12 : bedtime + length;
 
-  goalDisplay.textContent = `Your sleep goal is from ${bedtime} to ${length} for a total of ${length} hrs rest!`;
+  goalDisplay.textContent = `Your sleep goal is to sleep at ${bedtime} and wake up at ${wakeUpTime} for a total of ${length} hrs rest!`;
 });
 
 console.log(bedtime, length);
